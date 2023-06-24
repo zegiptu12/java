e4c6b46300f585b48b0095d765dcd6f7bdc88aab
zegiptu12 <patryk.jarosz17@gmail.com>
2023-06-24 15:39:50 +0200

    Dodanie kolejnej klasy integracyjnej Java

--- javaw/src/main/java/org/example/Main.java
+++ javaw/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
-public class Main
-{
-    public static void main( String[] args ) {
-    Book book = new Book("Larsson", "Millennium");
 
+import com.google.gson.Gson; // Add this import statement
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("Larsson", "Millennium");
+        Gson gson = new Gson();
+        String bookJson = gson.toJson(book);
+        System.out.println(" ");
+        System.out.println(bookJson);
     }
 }
