63df8c73f789594cb9df1c0e04b001573289dd37
zegiptu12 <patryk.jarosz17@gmail.com>
2023-06-24 14:08:40 +0200

    Dodanie biblioteki Gson

--- javaw/src/main/java/org/example/Book.java
+++ javaw/src/main/java/org/example/Book.java
@@ -1,4 +1,27 @@
 package org.example;
 
 public class Book {
+    String author;
+    String title;
+
+    public Book(String author, String title) {
+        this.author = author;
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
