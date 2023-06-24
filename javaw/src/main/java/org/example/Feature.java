0edca20dff4082f17630f2b89ca58fdedc55cb95
zegiptu12 <patryk.jarosz17@gmail.com>
2023-06-24 15:47:36 +0200

    integracja

--- javaw/src/main/java/org/example/Feature.java
+++ javaw/src/main/java/org/example/Feature.java
@@ -1,4 +1,28 @@
 package org.example;
 
 public class Feature {
+    int price;
+    float discount;
+
+    public Feature(int price, float discount) {
+        this.price = price;
+        this.discount = discount;
+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public double getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(float discount) {
+        this.discount = discount;
+    }
 }
--- javaw/src/main/java/org/example/Main.java
+++ javaw/src/main/java/org/example/Main.java
@@ -5,8 +5,11 @@ import com.google.gson.Gson; // Add this import statement
 public class Main {
     public static void main(String[] args) {
         Book book = new Book("Larsson", "Millennium");
+        Feature feature = new Feature( 1, 0.1F );
         Gson gson = new Gson();
         String bookJson = gson.toJson(book);
+        String priceJson = gson.toJson(feature);
+        System.out.println(priceJson);
         System.out.println(" ");
         System.out.println(bookJson);
     }
